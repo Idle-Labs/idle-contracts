296547de60a86b91005b608df30d54168f5c04ac
bugman <onlyforwin@gmail.com>
2020-01-11 21:39:11 +0100

    NEW: add IdleMcdBridge contract for migrating old idleSAIv1 to both idleSAIv2 and idleDAIv2. The contract has been provided by Dexlab team

--- migrations/2_deploy_indipendent_contracts.js
+++ migrations/2_deploy_indipendent_contracts.js
@@ -1,9 +1,34 @@
 var IdlePriceCalculator = artifacts.require("./IdlePriceCalculator.sol");
 var IdleFactory = artifacts.require("./IdleFactory.sol");
+var IdleMcdBridge = artifacts.require("./IdleMcdBridge.sol");
+
+const SAI = {
+  'live': '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+  'live-fork': '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359', // needed for truffle
+  'kovan': '0xC4375B7De8af5a38a93548eb8453a498222C4fF2',
+  'kovan-fork': '0xC4375B7De8af5a38a93548eb8453a498222C4fF2', // needed for truffle
+  'local': '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+  'local-fork': '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+  'test': '0xd6801a1DfFCd0a410336Ef88DeF4320D6DF1883e',
+  'coverage': '0xd6801a1DfFCd0a410336Ef88DeF4320D6DF1883e',
+};
+const DAI = {
+  'live': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  'live-fork': '0x6B175474E89094C44Da98b954EedeAC495271d0F', // needed for truffle
+  'kovan': '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa',
+  'kovan-fork': '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa', // needed for truffle
+  'local': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  'local-fork': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  'test': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  'coverage': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+};
 
 module.exports = async function(deployer, network, accounts) {
   console.log(`### Deploying indipendent contract on ${network}`);
 
   await deployer.deploy(IdlePriceCalculator);
   await deployer.deploy(IdleFactory);
+  await deployer.deploy(IdleMcdBridge, SAI[network], DAI[network]);
+
+  console.log(`IdleMcdBridge.address ${IdleMcdBridge.address}`);
 };
