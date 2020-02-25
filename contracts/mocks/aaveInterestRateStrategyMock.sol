313607f6d3d3c0eae646aa908b39f234d40bd8a6
bugman <onlyforwin@gmail.com>
2020-02-25 16:12:35 +0100

    NEW: add missing mocks for IdleAave test

---
+++ contracts/mocks/aaveInterestRateStrategyMock.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.5.11;
+
+// interfaces
+import "../interfaces/AaveInterestRateStrategy.sol";
+
+contract aaveInterestRateStrategyMock is AaveInterestRateStrategy {
+  uint256 public borrowRate;
+  uint256 public supplyRate;
+
+  constructor() public {
+  }
+
+  function getBaseVariableBorrowRate() external view returns (uint256) {
+    return borrowRate;
+  }
+  function calculateInterestRates(
+    address,
+    uint256,
+    uint256,
+    uint256,
+    uint256) external view
+  returns (uint256, uint256, uint256) {
+    return (supplyRate, borrowRate, borrowRate);
+  }
+
+  // mocked methods
+  function _setSupplyRate(uint256 _supplyRate) external {
+    supplyRate = _supplyRate;
+  }
+  function _setBorrowRate(uint256 _borrowRate) external {
+    borrowRate = _borrowRate;
+  }
+}
---
+++ contracts/mocks/aaveLendingPoolMock.sol
@@ -0,0 +1,36 @@
+pragma solidity 0.5.11;
+
+// interfaces
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../interfaces/AaveLendingPool.sol";
+
+contract aaveLendingPoolMock is AaveLendingPool {
+  address public dai;
+  address public aDai;
+
+  constructor (address _dai, address _aDai) public {
+    dai = _dai;
+    aDai = _aDai;
+  }
+  function deposit(address, uint256 _amount, uint16) external {
+    /* require(IERC20(dai).transferFrom(msg.sender, address(this), _amount), "Error during transferFrom"); // 1 DAI */
+    IERC20(aDai).transfer(msg.sender, _amount);
+  }
+  function getReserveData(address _reserve) external view returns (
+    uint256 totalLiquidity,
+    uint256 availableLiquidity,
+    uint256 totalBorrowsStable,
+    uint256 totalBorrowsVariable,
+    uint256 liquidityRate,
+    uint256 variableBorrowRate,
+    uint256 stableBorrowRate,
+    uint256 averageStableBorrowRate,
+    uint256 utilizationRate,
+    uint256 liquidityIndex,
+    uint256 variableBorrowIndex,
+    address aTokenAddress,
+    uint40 lastUpdateTimestamp
+  ) {
+
+  }
+}
