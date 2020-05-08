a9fc5ea1a83ec319c7defb7bc3e2afb76f5b6eb8
bugman <onlyforwin@gmail.com>
2020-05-08 15:25:17 +0200

    FIX: add IdleConverter in migration

--- migrations/2_deploy_indipendent_contracts.js
+++ migrations/2_deploy_indipendent_contracts.js
@@ -1,4 +1,5 @@
 var IdlePriceCalculator = artifacts.require("./IdlePriceCalculator.sol");
+var IdleConverter = artifacts.require("./IdleConverter.sol");
 const {creator, rebalancerManager, feeAddress, gstAddress} = require('./addresses.js');
 
 module.exports = async function(deployer, network, accounts) {
@@ -7,4 +8,5 @@ module.exports = async function(deployer, network, accounts) {
   }
   console.log(`### Deploying indipendent contract on ${network}`);
   await deployer.deploy(IdlePriceCalculator, {from: creator});
+  await deployer.deploy(IdleConverter, {from: creator});
 };
