44fe0da710291a89ee964123ee7a56853928b470
bugman <onlyforwin@gmail.com>
2020-05-19 01:15:01 +0200

    FIX: update migration

--- migrations/2_deploy_indipendent_contracts.js
+++ migrations/2_deploy_indipendent_contracts.js
@@ -3,10 +3,11 @@ var IdleConverter = artifacts.require("./IdleConverter.sol");
 const {creator, rebalancerManager, feeAddress, gstAddress} = require('./addresses.js');
 
 module.exports = async function(deployer, network, accounts) {
-  if (network === 'test') {
+  if (network === 'test' || network == 'coverage') {
     return;
   }
   console.log(`### Deploying indipendent contract on ${network}`);
-  await deployer.deploy(IdlePriceCalculator, {from: creator});
-  await deployer.deploy(IdleConverter, {from: creator});
+  // await deployer.deploy(IdlePriceCalculator, {from: creator});
+  // await deployer.deploy(IdleConverter, {from: creator});
+  await deployer.deploy(IdleConverter, {from: rebalancerManager});
 };
