8246c2083fcc386887fbc8d2dc12a856d4231801
bugman <onlyforwin@gmail.com>
2020-08-24 19:14:35 +0200

    FIX: add Pausable

--- contracts/IdleBatchConverter.sol
+++ contracts/IdleBatchConverter.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Deta
 import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/lifecycle/Pausable.sol";
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "./interfaces/IIdleTokenV3_1.sol";
 import "./interfaces/IIdleToken.sol";
@@ -24,19 +25,20 @@ contract IdleBatchConverter is Initializable, Ownable {
 
   function initialize(address _idleToken, address _newIdleToken) public initializer {
     Ownable.initialize(msg.sender);
+    Pausable.initialize(msg.sender);
     idleToken = _idleToken;
     newIdleToken = _newIdleToken;
     underlying = IIdleTokenV3_1(idleToken).token();
     IERC20(underlying).safeApprove(newIdleToken, uint256(-1));
   }
   // User should approve this contract first to spend IdleTokens idleToken
-  function deposit() external {
+  function deposit() external whenNotPaused {
     uint256 bal = IERC20(idleToken).balanceOf(msg.sender);
     IERC20(idleToken).safeTransferFrom(msg.sender, address(this), bal);
     batchDeposits[msg.sender][currBatch] = bal;
     batchTotals[currBatch] = batchTotals[currBatch].add(bal);
   }
-  function withdraw(uint256 batchId) external {
+  function withdraw(uint256 batchId) external whenNotPaused {
     require(currBatch != 0 && batchId < currBatch, 'Batch id invalid');
     uint256 deposited = batchDeposits[msg.sender][batchId];
     uint256 batchBal = batchRedeemedTotals[batchId];
@@ -46,7 +48,7 @@ contract IdleBatchConverter is Initializable, Ownable {
     batchDeposits[msg.sender][batchId] = 0;
     IERC20(newIdleToken).safeTransfer(msg.sender, share);
   }
-  function migrateFromToIdle(bool _skipRebalance) external returns (uint256) {
+  function migrateFromToIdle(bool _skipRebalance) external whenNotPaused returns (uint256) {
     IIdleToken(idleToken).redeemIdleToken(IERC20(idleToken).balanceOf(address(this)), true, new uint256[](0));
     uint256 minted = IIdleTokenV3_1(newIdleToken).mintIdleToken(
       IERC20(underlying).balanceOf(address(this)), _skipRebalance, address(0)
