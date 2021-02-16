b8255c3c7f55d874df895e408c09668fd5adcead
William Bergamo <william@idle.finance>
2021-02-16 16:41:25 +0100

    FIX: remove duplicated interfaces and migration 7

--- contracts/interfaces/IVester.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.5.16;
-pragma experimental ABIEncoderV2;
-
-interface IVester {
-  function setDelegate(address delegatee) external;
-}
--- contracts/interfaces/IVesterFactory.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.5.16;
-pragma experimental ABIEncoderV2;
-
-interface IVesterFactory {
-  function vestingContracts(address vestor) external returns (address);
-}
