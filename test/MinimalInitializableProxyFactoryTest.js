d4029292bd9cab990799f97874ceedeaa3b110d1
Andrea Franz <andrea@gravityblast.com>
2021-03-04 11:32:06 +0100

    add createAndCall method to MinimalInitializableProxyFactory

--- test/MinimalInitializableProxyFactoryTest.js
+++ test/MinimalInitializableProxyFactoryTest.js
@@ -14,17 +14,21 @@ contract("MinimalInitializableProxyFactory", ([_]) => {
 
     const initSig = "initialize(uint256)";
     const initData1 = web3.eth.abi.encodeParameters(['uint256'], [toBN(22)]);
-    const rec1 = await this.factory.create(implementation.address, initSig, initData1);
-    const proxy1 = await Foo.at(rec1.logs[0].args.proxy);
+    const res1 = await this.factory.createAndCall(implementation.address, initSig, initData1);
+    const proxy1 = await Foo.at(res1.logs[0].args.proxy);
 
     const initData2 = web3.eth.abi.encodeParameters(['uint256'], [toBN(33)]);
-    const rec2 = await this.factory.create(implementation.address, initSig, initData2);
-    const proxy2 = await Foo.at(rec2.logs[0].args.proxy);
+    const res2 = await this.factory.createAndCall(implementation.address, initSig, initData2);
+    const proxy2 = await Foo.at(res2.logs[0].args.proxy);
+
+    const res3 = await this.factory.create(implementation.address);
+    const proxy3 = await Foo.at(res3.logs[0].args.proxy);
 
 
     toBN(await implementation.x()).should.be.bignumber.equal(toBN("11"));
     toBN(await proxy1.x()).should.be.bignumber.equal(toBN("22"));
     toBN(await proxy2.x()).should.be.bignumber.equal(toBN("33"));
+    toBN(await proxy3.x()).should.be.bignumber.equal(toBN(""));
   });
 });
 
