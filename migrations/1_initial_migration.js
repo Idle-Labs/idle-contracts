47c5fd52118f294bde055392cc7c649520801ce0
William Bergamo <william@idle.finance>
2021-03-10 12:22:58 +0100

    FIX: do not run migrations in tests

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
+  if (network === 'test' || network == 'coverage') {
+    return;
+  }
   deployer.deploy(Migrations);
 };
