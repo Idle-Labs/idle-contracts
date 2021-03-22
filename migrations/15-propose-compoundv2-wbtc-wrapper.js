7554424ad58a02c5d9f262f084e05259e4927c5e
William Bergamo <william@idle.finance>
2021-03-22 15:57:08 +0100

    CHG: incorporate IDLE weth distribution and EB bounty payment

--- migrations/15-propose-compoundv2-wbtc-wrapper.js
+++ migrations/15-propose-compoundv2-wbtc-wrapper.js
@@ -1,120 +1,31 @@
 const rl = require("readline");
-const IdleAaveV2 = artifacts.require("IdleAaveV2.sol");
 const IdleCompoundV2 = artifacts.require("IdleCompoundV2.sol");
 const Idle = artifacts.require("Idle")
 const IdleTokenGovernance = artifacts.require("IdleTokenGovernance")
+const IdleController = artifacts.require("IdleController")
 const IERC20 = artifacts.require("IERC20Detailed.sol");
-const VesterFactory = artifacts.require("VesterFactory.sol");
-const Vester = artifacts.require("Vester");
 const addresses = require("./addresses");
+const { createProposal } = require("./create-proposal");
 const { time } = require('@openzeppelin/test-helpers');
 const BigNumber = require('bignumber.js');
-const IGovernorAlpha = artifacts.require("IGovernorAlpha");
 
 const toBN = v => new BigNumber(v.toString());
 const timelockDelay = 172800
-const TOKENS_HOLDER = "0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98";
-const addr0 = '0x0000000000000000000000000000000000000000';
-
-const prompt = (question) => {
-  const r = rl.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-  });
-
-  return new Promise((resolve, error) => {
-    r.question(question, answer => {
-      r.close()
-      resolve(answer)
-    });
-  })
-}
-
-const check = (a, b, message) => {
-  a = a.toString();
-  b = b.toString();
-  let [icon, symbol] = a.toString() === b ? ["✔️", "==="] : ["🚨🚨🚨", "!=="];
-  console.log(`${icon}  `, a, symbol, b, message ? message : "");
-}
-
-const checkIncreased = (a, b, message) => {
-  let [icon, symbol] = b.gt(a) ? ["✔️", ">"] : ["🚨🚨🚨", "<="];
-  console.log(`${icon}  `, a.toString(), symbol, b.toString(), message ? message : "");
-}
-
-const advanceBlocks = async n => {
-  for (var i = 0; i < n; i++) {
-    if (i === 0 || i % 100 === 0) {
-      process.stdout.clearLine();  // clear current text
-      process.stdout.cursorTo(0);
-      process.stdout.write(`waiting for ${n - i} blocks...`);
-    }
-
-    await time.advanceBlock();
-  }
-}
 
 module.exports = async (deployer, network, accounts) => {
   if (network === 'test' || network == 'coverage') {
     return;
   }
 
-  await deployer.deploy(IdleCompoundV2, addresses.WBTC.live, addresses.cWBTCV2.live, {from: creator});
-
+  await deployer.deploy(IdleCompoundV2, addresses.cWBTCV2.live, addresses.WBTC.live, {from: addresses.creator});
   const compoundV2Wrapper = await IdleCompoundV2.deployed();
   console.log("IdleCompoundV2 wbtc deployed at", compoundV2Wrapper.address);
-  await compoundV2Wrapper.setIdleToken(addresses.idleWBTCV4, {from: creator});
+  await compoundV2Wrapper.setIdleToken(addresses.idleWBTCV4, {from: addresses.creator});
   console.log('setIdleToken done');
-  console.log("************************************\n\n\n\n")
-  const getLatestPropsal = async gov => gov.proposalCount.call()
-  const createProposal = async (gov, founder, {targets, values, signatures, calldatas, description, from}, log) => {
-    console.log(`Proposing: ${log}`);
-    await gov.propose(targets, values, signatures, calldatas, description,
-      {from}
-    );
-
-    console.log("proposal created");
-
-    if (network === 'live') {
-      return;
-    }
-
-    // need 1 block to pass before being able to vote but less than 10
-    await advanceBlocks(2);
-    const proposalId = await getLatestPropsal(gov);
-    await gov.castVote(proposalId, true, {from: founder});
-    console.log('voted');
-
-    // Need to advance 3d in blocs + 1
-    await advanceBlocks(17281);
-
-    await gov.queue(proposalId);
-    console.log('queued');
-
-    await time.increase(timelockDelay+100)
-    console.log("time increased")
-    await advanceBlocks(1)
-    console.log("advanced 1")
-
-    await gov.execute(proposalId);
-    console.log('executed');
-    await advanceBlocks(2);
-  };
-
-  const founder = "0x3675D2A334f17bCD4689533b7Af263D48D96eC72";
-  const govInstance = await IGovernorAlpha.at('0x2256b25CFC8E35c3135664FD03E77595042fe31B')
+  console.log("************************************\n\n\n\n");
 
   const cTokenV2Address = addresses.cWBTCV2.live;
   const underlyingTokenAddress = addresses.WBTC.live;
-
-  const idleInstance = await Idle.at(addresses.IDLE)
-  const vesterFactory = await VesterFactory.at("0xbF875f2C6e4Cc1688dfe4ECf79583193B6089972")
-  const founderVesting = await vesterFactory.vestingContracts.call(founder);
-  const vesterFounder = await Vester.at(founderVesting);
-
-  await idleInstance.delegate(founder, {from: founder});
-  await vesterFounder.setDelegate(founder, {from: founder});
 
   const targets = [];
   const values = [];
@@ -134,7 +45,6 @@ module.exports = async (deployer, network, accounts) => {
     const attrs = idleTokens[idleTokenName];
     const idleToken = await IdleTokenGovernance.at(attrs.idleTokenAddress);
     const res = await idleToken.getAPRs();
-
     let tokens = res["0"].map(v => v.toString());
     let wrappers = [];
 
@@ -149,35 +59,74 @@ module.exports = async (deployer, network, accounts) => {
 
     const params = [
       ['address[]', 'address[]', 'uint256[]', 'bool'],
-      [tokens, wrappers, [20000, 20000, 10000, 40000, 10000], true]
+      [tokens, wrappers, [20000, 20000, 60000], true]
     ];
 
+    console.log("calldatasParams", params);
+
+    // Add one action to add the new wbtcv2 wrapper
     targets.push(attrs.idleTokenAddress);
     values.push(toBN("0"));
     signatures.push("setAllAvailableTokensAndWrappers(address[],address[],uint256[],bool)");
     calldatas.push(web3.eth.abi.encodeParameters(...params));
   }
 
-  console.log("targets", targets)
-  console.log("signatures", signatures)
-  console.log("calldatas", calldatas)
 
-  let propName = 'add compoundv2 wrapper';
+  // Add one action to withdraw 3500 Idle from ecosystem fund
+  const ONE = toBN('1e18');
+  console.log(ONE.toString())
+  targets.push(addresses.ecosystemFund);
+  values.push(toBN('0'));
+  signatures.push('transfer(address,address,uint256)');
+  calldatas.push(
+    web3.eth.abi.encodeParameters(
+      ['address', 'address', 'uint256'],
+      [addresses.IDLE, addresses.bountyAddressForEB, toBN('3500').times(ONE)]
+    )
+  );
+
+  // Add one action to support IDLE distribution to idleWETH
+  targets.push(addresses.idleController);
+  values.push(toBN('0'));
+  signatures.push('_supportMarkets(address[])');
+  calldatas.push(
+    web3.eth.abi.encodeParameters(
+      ['address[]'],
+      [
+        [addresses.idleWETHV4]
+      ]
+    )
+  );
+
+  // Add one action to activate IDLE distribution to idleWETH
+  targets.push(addresses.idleController);
+  values.push(toBN('0'));
+  signatures.push('_addIdleMarkets(address[])');
+  calldatas.push(
+    web3.eth.abi.encodeParameters(
+      ['address[]'],
+      [
+        [addresses.idleWETHV4]
+      ]
+    )
+  );
+
   let proposal = {
     targets: targets,
     values: values,
     signatures: signatures,
     calldatas: calldatas,
-    description: propName,
-    from: founder
+    description: '#IIPX: add compoundV2 wrapper for idleWBTC and enable IDLE distribution for idleWETH. Pay 3500 IDLE bounty to Emiliano #IIP-4 Treasury committee and aave v2 fix \n https://gov.idle.finance/t/enable-idle-farming-for-the-weth-pool/348 - https://gov.idle.finance/t/compound-wbtc-migration-required-adjustments/351 - https://gov.idle.finance/t/bounty-to-emiliano-a-chance-to-setup-a-developer-mining-program/251',
+    from: addresses.forkProposer
   }
 
-  await createProposal(govInstance, founder, proposal, propName);
+  await createProposal(network, proposal);
+
   if (network === 'live') {
     return;
   }
 
-  await web3.eth.sendTransaction({ from: TOKENS_HOLDER, to: addresses.timelock, value: "1000000000000000000" });
+  await web3.eth.sendTransaction({ from: addresses.whale, to: addresses.timelock, value: "1000000000000000000" });
 
   const setAllocationsAndRebalance = async (idleToken, newWrapperAllocation) => {
     const tokens = (await idleToken.getAPRs()).addresses;
@@ -217,8 +166,20 @@ module.exports = async (deployer, network, accounts) => {
     };
   }
 
+  // Test stuff
   for (const idleTokenName in idleTokens) {
+    const user = addresses.mintRedeemTestUser;
     const attrs = idleTokens[idleTokenName];
+    // Test that idleWETH has a speed
+    const idleCtrl = await IdleController.at(addresses.idleController);
+    const idle = await IERC20.at(addresses.IDLE);
+    console.log('balance', (await idle.balanceOf(addresses.bountyAddressForEB, {from: user})).toString());
+    console.log('getAllMarkets', await idleCtrl.getAllMarkets({from: user}));
+    console.log('speed idleDAIV4', (await idleCtrl.idleSpeeds(addresses.idleDAIV4, {from: user})).toString());
+    console.log('speed idleUSDCV4', (await idleCtrl.idleSpeeds(addresses.idleUSDCV4, {from: user})).toString());
+    console.log('speed idleWBTCV4', (await idleCtrl.idleSpeeds(addresses.idleWBTCV4, {from: user})).toString());
+    console.log('speed idleWETHV4', (await idleCtrl.idleSpeeds(addresses.idleWETHV4, {from: user})).toString());
+
     console.log("\n\n********************************* testing", idleTokenName);
 
     const idleToken = await IdleTokenGovernance.at(attrs.idleTokenAddress);
@@ -228,22 +189,21 @@ module.exports = async (deployer, network, accounts) => {
     await setAllocationsAndRebalance(idleToken, 0);
 
     // test mint and redeem
-    const user = '0xF1363D3D55d9e679cC6aa0a0496fD85BDfCF7464';
     const underlying = await idleToken.token();
     console.log('underlying', underlying);
     const underlyingContract = await IERC20.at(underlying);
     const tokenDecimals = await underlyingContract.decimals();
-    console.log('tokenDecimals', tokenDecimals);
+    console.log('tokenDecimals', tokenDecimals.toString());
     const oneToken = toBN(`1e${tokenDecimals}`);
     const oneIdleToken = toBN(`1e18`);
-    const amount = oneToken.times('100');
-    await underlyingContract.transfer(user, amount, {from: TOKENS_HOLDER}); // whale
+    const amount = oneToken.times('10');
+    await underlyingContract.transfer(user, amount, {from: addresses.whale}); // whale
     console.log('transfer complete');
 
     await underlyingContract.approve(idleToken.address, amount, {from: user});
     console.log('##### balance token user pre: ', toBN(await underlyingContract.balanceOf(user)).div(oneToken).toString());
     console.log('##### balance idleToken user pre: ', toBN(await idleToken.balanceOf(user)).div(oneIdleToken).toString());
-    await idleToken.mintIdleToken(amount, true, addr0, {from: user});
+    await idleToken.mintIdleToken(amount, true, addresses.addr0, {from: user});
     console.log('##### balance token user post: ', toBN(await underlyingContract.balanceOf(user)).div(oneToken).toString());
     console.log('##### balance idleToken user post: ', toBN(await idleToken.balanceOf(user)).div(oneIdleToken).toString());
     await idleToken.redeemIdleToken(await idleToken.balanceOf(user), {from: user});
--- migrations/addresses.js
+++ migrations/addresses.js
@@ -391,6 +391,7 @@ const idleMultisig = '0xaDa343Cb6820F4f5001749892f6CAA9920129F2A';
 const IDLE = '0x875773784Af8135eA0ef43b5a374AaD105c5D39e';
 const timelock = '0xD6dABBc2b275114a2366555d6C481EF08FDC2556';
 const proxyAdmin = '0x7740792812A00510b50022D84e5c4AC390e01417';
+const proxyAdminETH = '0xc2ff102E62027DE1205a7EDd4C8a8F58C1E5e3e8';
 
 const allIdleTokens = [
   idleDAIV4,
@@ -471,6 +472,7 @@ module.exports = {
   IDLE: IDLE,
   timelock: timelock,
   proxyAdmin: proxyAdmin,
+  proxyAdminETH: proxyAdminETH,
   allIdleTokens: allIdleTokens,
   aaveAddressesProvider: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
   minimalInitializableProxyFactory,
@@ -485,5 +487,12 @@ module.exports = {
   idleAaveV2USDCSafe,
   idleAaveV2USDTSafe,
   addr0: '0x0000000000000000000000000000000000000000',
-  idleController: '0x275DA8e61ea8E02d51EDd8d0DC5c0E62b4CDB0BE'
+  idleController: '0x275DA8e61ea8E02d51EDd8d0DC5c0E62b4CDB0BE',
+  governorAlpha: '0x2256b25CFC8E35c3135664FD03E77595042fe31B',
+  ecosystemFund: '0xb0aA1f98523Ec15932dd5fAAC5d86e57115571C7',
+  vesterFactory: '0xbF875f2C6e4Cc1688dfe4ECf79583193B6089972',
+  whale: '0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE',
+  forkProposer: '0x3675D2A334f17bCD4689533b7Af263D48D96eC72',
+  bountyAddressForEB: '0x394495a3800d1504b5686d398836baefebd0c5b7',
+  mintRedeemTestUser: '0xF1363D3D55d9e679cC6aa0a0496fD85BDfCF7464'
 };
