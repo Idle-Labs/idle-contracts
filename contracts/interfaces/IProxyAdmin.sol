84a8b632c49bcbf2858bc55d2390c1750cfc645b
bugduino <william@idle.finance>
2021-03-31 15:31:34 +0200

    FIX: update address and add some method in IProxyAdmin interface

--- contracts/interfaces/IProxyAdmin.sol
+++ contracts/interfaces/IProxyAdmin.sol
@@ -4,4 +4,6 @@ interface IProxyAdmin {
   function owner() external returns (address);
   function transferOwnership(address) external;
   function changeProxyAdmin(address proxy, address newAdmin) external;
+  function getProxyAdmin(address proxy) external view returns (address);
+  function upgrade(address proxy, address implementation) external;
 }
