f059b51fbd6227b1ac803358872194add94e202e
bugduino <william@idle.finance>
2021-04-08 17:46:37 +0200

    FIX: remove unused variable

--- contracts/mocks/IdleTokenHelperMock.sol
+++ contracts/mocks/IdleTokenHelperMock.sol
@@ -10,7 +10,6 @@ contract IdleTokenHelperMock is IdleTokenHelperNoConst {
   constructor(address _idle, address _comp, address _uni_router) public {
     IDLE = _idle;
     COMP = _comp;
-    UNI_ROUTER_V2 = _uni_router;
   }
 
   function sellGovTokens(address _idleToken, uint256[] calldata _minTokenOut) external {
