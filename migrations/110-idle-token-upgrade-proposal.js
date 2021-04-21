6f63e033c2bc426cb50dce4ce0a33af0872f6e50
Andrea Franz <andrea@gravityblast.com>
2021-04-21 16:00:14 +0200

    remove duplicated code

--- migrations/110-idle-token-upgrade-proposal.js
+++ migrations/110-idle-token-upgrade-proposal.js
@@ -20,32 +20,18 @@ module.exports = async (deployer, network, accounts) => {
   }
 
   let idleTokenImplementationAddress = '';
-  let idleTokenHelperAddress = '';
+
+  await deployer.deploy(IdleTokenHelper);
+  const idleTokenHelper = await IdleTokenHelper.deployed();
+  const idleTokenHelperAddress = idleTokenHelper.address;
+  console.log("idle token helper deployed at", idleTokenHelperAddress);
 
   if (network === "local") {
     await deployer.deploy(IdleTokenGovernance);
     const idleTokenImplementation = await IdleTokenGovernance.deployed();
     idleTokenImplementationAddress = idleTokenImplementation.address;
     console.log("implementation deployed at", idleTokenImplementationAddress)
-
-    await deployer.deploy(IdleTokenHelper);
-    const idleTokenHelper = await IdleTokenHelper.deployed();
-    idleTokenHelperAddress = idleTokenHelper.address;
-    console.log("idle token helper deployed at", idleTokenHelperAddress);
   }
-
-  const founder = "0x3675D2A334f17bCD4689533b7Af263D48D96eC72";
-  const proxyAdmin = '0x7740792812A00510b50022D84e5c4AC390e01417';
-  const idleWETHProxyAdmin = '0xc2ff102E62027DE1205a7EDd4C8a8F58C1E5e3e8';
-  const govInstance = await IGovernorAlpha.at('0x2256b25CFC8E35c3135664FD03E77595042fe31B')
-
-  const idleInstance = await Idle.at(addresses.IDLE)
-  const vesterFactory = await VesterFactory.at("0xbF875f2C6e4Cc1688dfe4ECf79583193B6089972")
-  const founderVesting = await vesterFactory.vestingContracts.call(founder);
-  const vesterFounder = await Vester.at(founderVesting);
-
-  await idleInstance.delegate(founder, {from: founder});
-  await vesterFounder.setDelegate(founder, {from: founder});
 
   const allIdleTokens = [
     addresses.idleDAIV4,
@@ -63,7 +49,7 @@ module.exports = async (deployer, network, accounts) => {
   let targets = allIdleTokens.map(a => addresses.proxyAdmin);
   let values = allIdleTokens.map(a => toBN("0"));
   let signatures = allIdleTokens.map(a => "upgradeAndCall(address,address,bytes)");
-  let initMethodToCall = web3.eth.abi.encodeFunctionCall({
+  const initMethodToCall = web3.eth.abi.encodeFunctionCall({
     name: "_init(address)",
     type: "function",
     inputs: [
@@ -95,16 +81,14 @@ module.exports = async (deployer, network, accounts) => {
   console.log("callDatas", callDatas);
 
   const proposal = {
-    targets: targets.map(t => proxyAdmin),
+    targets: targets.map(t => addresses.proxyAdmin),
     values: values,
     signatures: signatures,
     calldatas: callDatas,
     description: propName,
-    from: founder,
+    from: addresses.forkProposer,
   };
 
-
-
   await askToContinue("continue?");
-  await createProposal(govInstance, proposal);
+  await createProposal(network, proposal);
 }
