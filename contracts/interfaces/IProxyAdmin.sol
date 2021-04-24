f7084b8bcc157c9f5381ab2c5af7e146040acdca
bugduino <william@idle.finance>
2021-04-24 18:35:41 +0200

    FIX: port IProxyAdmin changes

--- contracts/interfaces/IProxyAdmin.sol
+++ contracts/interfaces/IProxyAdmin.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.5.16;
 
 interface IProxyAdmin {
+  function owner() external returns (address);
   function transferOwnership(address) external;
   function changeProxyAdmin(address proxy, address newAdmin) external;
+  function getProxyAdmin(address proxy) external view returns (address);
   function upgrade(address proxy, address implementation) external;
-  function upgradeAndCall(address, address, bytes calldata) external payable;
 }
